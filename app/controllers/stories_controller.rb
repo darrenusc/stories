75c41052cee0439fc2e5b2babf87736782c1d16b
Darren Chan <darrenusc@gmail.com>
2017-11-18 16:12:48 -0800

    Add flash notice for create, update, and destroy actions in Story controller.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -20,17 +20,20 @@ class StoriesController < ApplicationController
   def create
     @story = Story.new(story_params)
     @story.save
+    flash.notice = "Story '#{@story.title}' Created!"
     redirect_to story_path(@story)
   end
 
   def update
     @story = Story.find(params[:id])
     @story.update(story_params)
+    flash.notice = "Story '#{@story.title}' Updated!"
     redirect_to story_path(@story)
   end
 
   def destroy
     @story = Story.find(params[:id]).destroy
+    flash.notice = "Story '#{@story.title}' Deleted!"
     redirect_to stories_path
   end
 end
