6d669667697a7be9082ac701f0e29118a2cac169
Darren Chan <darrenusc@gmail.com>
2017-12-06 11:09:14 -0800

    Install Devise and add User model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Define default url options 
+  # config.action_mailer.default_url_options = { host: 'https://glacial-peak-32677.herokuapp.com/stories' }
 end
